cae4766ed5e201eb28799ecdbabe66e8e648af08
Buddy <adityasahai925@gmail.com>
2020-05-07 01:51:19 +0530

    Getting Movie Details

--- Data.py
+++ Data.py
@@ -45,20 +45,65 @@ class Data:
                 serial_num = line[0][1:-1].strip()
                 release_year = line[3][1:-1].strip()
                 rating = line[4][1:-2].strip()
-                return serial_num, release_year, rating
+                link = line[2][1:-1].strip()
+                return serial_num, release_year, rating,link
 
         return False
 
         self.file.close()
+
+    def get_movie_details(self, word):
+        """Used when the actor wants more information on a movie."""
+        url = self.give_movie_info(word)[3]
+        response = requests.get(url)
+        plot = BeautifulSoup(response.content, "html.parser").find(class_="plot_summary")
+
+        desc = plot.find(class_="summary_text").get_text().strip()
+
+        credit_box = plot.find_all(class_="credit_summary_item")
+        director = credit_box[0].a.get_text()
+        writers = credit_box[1].find_all("a")
+        actors = credit_box[1].find_all("a")
+
+        for index, writer in enumerate(writers):
+            writers[index] = writer.get_text()
+
+        for index, actor in enumerate(actors):
+            actors[index] = actor.get_text()
+
+        # print(plot.prettify())
+        # print(desc)
+        # print(director)
+        # print(writers)
+        # print(actors)
+
+        return desc, director, writers, actors
+
 
 if __name__ == "__main__":
     obj = Data()
     # obj.restore_data()
 
     movie = input("Enter any movie: ")
-    info = obj.give_movie_info(movie)
 
-    if info:
-        print(f"Rank: {info[0]}")
-        print(f"Movie Release Year : {info[1]}")
-        print(f"Rating: {info[2]}")
+    # info = obj.give_movie_info(movie)
+    #
+    # if info:
+    #     print(f"Rank: {info[0]}")
+    #     print(f"Movie Release Year : {info[1]}")
+    #     print(f"Rating: {info[2]}")
+    # print(obj.get_web_actor(movie))
+
+    movie_info = obj.get_movie_details(movie)
+
+    print(movie_info[0])
+
+    print(f"\nDirector:\n\t{movie_info[1]}")
+
+    print("Writers:")
+    for writer in movie_info[2]:
+        print(f"\t{writer.title()}")
+
+    print("Actors:")
+    for actor in movie_info[3]:
+        print(f"\t{actor.title()}")
